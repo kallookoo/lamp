3f93989492d99972f8b2f97a9f115578bc92c798
kallookoo <kallookoo@users.noreply.github.com>
2021-11-06 18:00:45 +0100

    Add extras folder, possible improvements

---
+++ extras/mysql-autobackup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if [ `id -u` -ne 0 ]; then
+  echo "Please, run this with the root user or sudo."
+  exit 1
+fi
+
+MYSQL_DIRECTORY="${1:-}"
+if [ -z "$MYSQL_DIRECTORY" ]; then
+  echo "Undefined directory arguments"
+  echo "Usage: mysql-autobackup.sh directory"
+  exit 1
+fi
+MYSQL_DIRECTORY="$(echo "$MYSQL_DIRECTORY" | sed -e 's@/$@@')"
+if [ ! -d "$MYSQL_DIRECTORY" ]; then
+  mkdir -p "$MYSQL_DIRECTORY"
+fi
+
+# Usage: in_array string ${array[@]}
+function in_array() {
+  [ `printf '%s\n' "${@}" | grep -cx -- "${1}"` -gt 1 ] && return 0
+  return 1
+}
+
+MYSQL_EXCLUDE_DATABASES=(
+  information_schema
+  mysql
+  performance_schema
+  phpmyadmin
+  sys
+)
+
+mysql -se 'show databases' | while read database; do
+  if ! in_array "$database" ${MYSQL_EXCLUDE_DATABASES[@]}; then
+    if [ -f "$MYSQL_DIRECTORY/$database.sql" ]; then
+      gzip -fk9 "$MYSQL_DIRECTORY/$database.sql"
+    fi
+    mysqldump --databases "$database" > "$MYSQL_DIRECTORY/$database.sql"
+  fi
+done
