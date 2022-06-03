b3b8ba11720cc10d7656053223d6ddaad0d5d7b1
kallookoo <kallookoo@users.noreply.github.com>
2022-06-03 07:10:40 +0200

    cleaning and small customizations

--- debian/phpmyadmin/config.inc.php
+++ debian/phpmyadmin/config.inc.php
@@ -1,49 +1,51 @@
-<?php
-/* vim: set expandtab sw=4 ts=4 sts=4: */
+<?php // phpcs:disable WordPress
 /**
- * phpMyAdmin sample configuration, you can use it as base for
- * manual configuration. For easier setup you can use setup/
- *
- * All directives are explained in documentation in the doc/ folder
- * or at <https://docs.phpmyadmin.net/>.
+ * All directives are explained at <https://docs.phpmyadmin.net/>.
  *
  * @package PhpMyAdmin
  */
 
 /**
- * This is needed for cookie based authentication to encrypt password in
- * cookie. Needs to be 32 chars long.
+ * Global phpMyAdmin configurations
  */
-$cfg['blowfish_secret'] = 'LF8jy9eQ0DgKaQ3WdH0tLF8jy9eQ0DgKaQ3WdH0t'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['TempDir']                       = '/tmp/';
+$cfg['ThemeManager']                  = false;
+$cfg['MaxRows']                       = 50;
+$cfg['SendErrorReports']              = 'never';
+$cfg['TitleDefault']                  = '@HTTP_HOST@';
+$cfg['TitleServer']                   = $cfg['TitleDefault'];
+$cfg['TitleDatabase']                 = '@HTTP_HOST@ : @DATABASE@';
+$cfg['TitleTable']                    = '@HTTP_HOST@ : @DATABASE@ : @TABLE@';
+$cfg['ShowDatabasesNavigationAsTree'] = false;
+$cfg['VersionCheck']                  = false;
+$cfg['QueryHistoryDB']                = true;
+$cfg['Export']['method']              = 'custom-no-form';
+$cfg['Console']['Height']             = 300;
 
 /**
  * Servers configuration
  */
-$i = 0;
 
-/**
- * First server
- */
-$i++;
-/* Authentication type */
+/** Set to one because not exists another server. */
+$i = 1;
+
+/** Authentication type */
 $cfg['Servers'][ $i ]['auth_type'] = 'config';
 $cfg['Servers'][ $i ]['user']      = 'root';
 $cfg['Servers'][ $i ]['password']  = 'root';
 /* Server parameters */
-$cfg['Servers'][ $i ]['host']            = 'localhost';
-$cfg['Servers'][ $i ]['compress']        = false;
-$cfg['Servers'][ $i ]['AllowNoPassword'] = false;
-$cfg['Servers'][ $i ]['hide_db']         = '^((information|performance)_schema|mysql|phpmyadmin|sys)$';
+$cfg['Servers'][ $i ]['host']    = 'localhost';
+$cfg['Servers'][ $i ]['hide_db'] = '^((information|performance)_schema|mysql|phpmyadmin|sys)$';
 
 /**
  * Configuration storage settings.
  */
 
-/* User used to manipulate with storage */
+/** User used to manipulate with storage */
 $cfg['Servers'][ $i ]['controluser'] = 'pma';
 $cfg['Servers'][ $i ]['controlpass'] = 'PMA_PASSWORD';
 
-/* Storage database and tables */
+/** Storage database and tables */
 $cfg['Servers'][ $i ]['pmadb']             = 'phpmyadmin';
 $cfg['Servers'][ $i ]['bookmarktable']     = 'pma__bookmark';
 $cfg['Servers'][ $i ]['relation']          = 'pma__relation';
@@ -64,17 +66,3 @@ $cfg['Servers'][ $i ]['savedsearches']     = 'pma__savedsearches';
 $cfg['Servers'][ $i ]['central_columns']   = 'pma__central_columns';
 $cfg['Servers'][ $i ]['designer_settings'] = 'pma__designer_settings';
 $cfg['Servers'][ $i ]['export_templates']  = 'pma__export_templates';
-
-/**
- * End of servers configuration
- */
-
-$cfg['TempDir']                       = '/tmp/';
-$cfg['ThemeManager']                  = false;
-$cfg['MaxRows']                       = 50;
-$cfg['SendErrorReports']              = 'never';
-$cfg['TitleDefault']                  = '@HTTP_HOST@';
-$cfg['TitleServer']                   = '@HTTP_HOST@';
-$cfg['TitleDatabase']                 = '@HTTP_HOST@ : @DATABASE@';
-$cfg['TitleTable']                    = '@HTTP_HOST@ : @DATABASE@ : @TABLE@';
-$cfg['ShowDatabasesNavigationAsTree'] = false;
