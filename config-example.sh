15983f4fbe51d6d05b0ff30da7853d93285316e5
sergio <sergio@MacBookPro.local>
2022-07-11 12:50:36 +0200

    Change custom config method

---
+++ config-example.sh
@@ -0,0 +1,28 @@
+# shellcheck shell=bash
+
+# Available configurations
+#
+# Empty values use the predefined
+# Uncomment the variable and define the value
+#
+
+# # Custom domain for lamp
+# LAMP_CONFIG_FQDN=""
+# # Custom TLD to use
+# LAMP_CONFIG_TLD=""
+# # Define the IP to use to configure the bind9
+# LAMP_CONFIG_IP_ADDRESS=""
+# # Default version of PHP
+# LAMP_CONFIG_PHP_VERSION=""
+# # Directory to use for all domains ( virtualhosts )
+# LAMP_CONFIG_VIRTUALHOSTS_DIRECTORY=""
+# # Default language for phpMyAdmin
+# LAMP_CONFIG_PMA_LANG=""
+# # Cron type to execute the auto update.
+# # Possible values: hourly, daily, weekly, monthly
+# # Default value is daily
+# LAMP_CONFIG_PMA_CRON_UPGRADE=""
+# # MariaDB version
+# # See available version in https://mariadb.org
+# # Default value is 10.8 ( checked in June 2022 )
+# LAMP_CONFIG_MARIADB_VERSION=""
