90b61b303004050a26e95fe811b321954eb97b25
kallookoo <kallookoo@users.noreply.github.com>
2023-07-18 21:50:15 +0200

    Remplace the null redirection

--- ubuntu/repositories.sh
+++ ubuntu/repositories.sh
@@ -6,7 +6,7 @@ apt_install software-properties-common
 
 function add_ppa_repository() {
   local repo="${1}"
-  if ! grep -q "^deb.*${repo}" /etc/apt/sources.list.d/*.list &>/dev/null; then
-    add-apt-repository -y --no-update "ppa:${repo}" &>/dev/null
+  if ! grep -q "^deb.*${repo}" /etc/apt/sources.list.d/*.list >/dev/null 2>&1; then
+    add-apt-repository -y --no-update "ppa:${repo}" >/dev/null 2>&1
   fi
 }
