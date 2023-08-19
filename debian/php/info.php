c4a3b4e8844f17f7844210d689336c410746f66e
kallookoo <kallookoo@users.noreply.github.com>
2023-08-19 22:20:27 +0200

    Add favicon fallback

--- debian/php/info.php
+++ debian/php/info.php
@@ -1,23 +1,31 @@
 <?php // phpcs:disable
-ob_start();
-?>
-<ul style="list-style: none; margin: 16px 0; padding: 0; text-align: center; display: flex; justify-content: center;">
-<?php
+$menu = '<ul style="list-style: none; margin: 16px 0; padding: 0; text-align: center; display: flex; justify-content: center;">';
 foreach ( glob( '/etc/php/*' ) as $version ) {
-	?>
-	<li style="margin: 0 10px;">
-	<?php
-	$version = basename( $version );
+	$version = substr( $version, 9 );
 	$style   = 'color: #444;';
 	if ( false !== strpos( PHP_VERSION, $version ) ) {
 		$style .= ' font-weight: bold;';
 	}
-	printf( '<a style="%2$s" href="?version=%1$s">Version %1$s</a></li>', $version, $style );
+	$menu .= sprintf( '<li style="margin: 0 10px;"><a style="%2$s" href="?version=%1$s">Version %1$s</a></li>', $version, $style );
 }
-?>
-</ul>
-<?php
-$menu = ob_get_clean();
+$menu .= '</ul>';
+
+$meta = '
+<link rel="icon" type="image/svg+xml" sizes="any" href="https://www.php.net/favicon.svg">
+<link rel="icon" type="image/png" sizes="196x196" href="https://www.php.net/favicon-196x196.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://www.php.net/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://www.php.net/favicon-16x16.png">
+<link rel="shortcut icon" href="https://www.php.net/favicon.ico">
+';
+
 ob_start();
 phpinfo();
-echo str_replace( '<body>', "<body>{$menu}", ob_get_clean() );
+echo preg_replace(
+	[ '/<body>/', '/<\/head>/' ],
+	[
+		sprintf( '<body>%s', $menu ),
+		sprintf( '%s</head>', $meta ),
+	],
+	ob_get_clean(),
+	1
+);
