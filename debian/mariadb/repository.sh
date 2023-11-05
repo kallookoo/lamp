f31435fc20c0de0e72318999ba1d848f79681162
kallookoo <kallookoo@users.noreply.github.com>
2023-11-05 16:58:09 +0100

    Fix detect MariaDB repository

--- debian/mariadb/repository.sh
+++ debian/mariadb/repository.sh
@@ -7,13 +7,13 @@ LAMP_MARIADB_REPO_URL="https://archive.mariadb.org/mariadb-${LAMP_MARIADB_VERSIO
 find /etc/apt/sources.list.d -type f -name "mariadb*" -delete
 if [ ! -f "/etc/apt/sources.list.d/mariadb-${LAMP_MARIADB_VERSION}.list" ]
 then
-  if ! curl -sIL "${LAMP_MARIADB_REPO_URL}/dists/${LAMP_DISTRO_CODENAME}/" | grep -q "200 Found"
+  if curl -sIL --fail "${LAMP_MARIADB_REPO_URL}/dists/${LAMP_DISTRO_CODENAME}/" >/dev/null 2>&1
   then
-    console_log "${LAMP_INCLUDE_NAME}" "Missing MariaDB ${LAMP_MARIADB_VERSION} version for \"${LAMP_DISTRO_CODENAME}\" in https://mariadb.org"
-    console_log "${LAMP_INCLUDE_NAME}" "The OS version will be used"
-  else
     find /etc/apt/sources.list.d -type f -name "mariadb*" -delete
     wget -q -O- https://mariadb.org/mariadb_release_signing_key.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/mariadb.gpg
     echo "deb ${LAMP_MARIADB_REPO_URL} $LAMP_DISTRO_CODENAME main" > "/etc/apt/sources.list.d/mariadb-${LAMP_MARIADB_VERSION}.list"
+  else
+    console_log "${LAMP_INCLUDE_NAME}" "Missing MariaDB ${LAMP_MARIADB_VERSION} version for \"${LAMP_DISTRO_CODENAME}\" in https://mariadb.org"
+    console_log "${LAMP_INCLUDE_NAME}" "The OS version will be used"
   fi
 fi
