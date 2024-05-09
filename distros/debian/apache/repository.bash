f95f27667c6a803fffc08771a5732c5ea58e32be
kallookoo <kallookoo@users.noreply.github.com>
2024-05-09 20:48:15 +0200

    Fix deb.sury keys

--- distros/debian/apache/repository.bash
+++ distros/debian/apache/repository.bash
@@ -2,7 +2,11 @@
 # Declare the Apache Repository
 #
 
+if ! package_exists debsuryorg-archive-keyring; then
+  curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+  dpkg -i /tmp/debsuryorg-archive-keyring.deb
+fi
+
 if [[ ! -f /etc/apt/sources.list.d/apache2.list ]]; then
-  wget -q https://packages.sury.org/apache2/apt.gpg -O /etc/apt/trusted.gpg.d/apache2.gpg
-  echo "deb https://packages.sury.org/apache2/ $LAMP_DISTRO_CODENAME main" >/etc/apt/sources.list.d/apache2.list
+  echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-apache2.gpg] https://packages.sury.org/apache2/ $LAMP_DISTRO_CODENAME main" >/etc/apt/sources.list.d/apache2.list
 fi
