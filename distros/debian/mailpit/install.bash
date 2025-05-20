91ae77e32206b94947d9e130337953eeab56c740
kallookoo <kallookoo@users.noreply.github.com>
2025-05-20 14:42:00 +0200

    Change mailpit installation mode

--- distros/debian/mailpit/install.bash
+++ distros/debian/mailpit/install.bash
@@ -2,23 +2,34 @@
 # MailPit Installer
 #
 
+mkdir -p /tmp/lamp-mailpit/bin
 systemctl stop mailpit >/dev/null 2>&1
-
-console_log "The installer will be executed"
-bash < <(curl -sL https://raw.githubusercontent.com/axllent/mailpit/develop/install.sh) | while read -r line; do
-  console_log "$line"
-done
-unset line
-
-if ! grep -q 'mailpit' /etc/passwd; then
-  adduser --system --group mailpit
+if download https://raw.githubusercontent.com/axllent/mailpit/develop/install.sh "/tmp/lamp-mailpit/install.sh"; then
+  sh "/tmp/lamp-mailpit/install.sh" --install-path "/tmp/lamp-mailpit/bin" --token "${LAMP_GITHUB_TOKEN:-}" 2>&1 | tee "/tmp/lamp-mailpit/install.log" >/dev/null
+  if [[ -x "/tmp/lamp-mailpit/bin/mailpit" ]]; then
+    cp -f "/tmp/lamp-mailpit/bin/mailpit" /usr/local/bin/mailpit
+    chmod +x /usr/local/bin/mailpit
+    if ! grep -q 'mailpit' /etc/passwd; then
+      adduser --system --group mailpit
+    fi
+    mkdir -p /etc/mailpit
+    chown "mailpit:mailpit" /etc/mailpit
+    cp -f "$LAMP_DISTRO_PATH/mailpit/mailpit.env" /etc/mailpit/mailpit.env
+    if [[ -f "$LAMP_PATH/config/mailpit.env" ]]; then
+      cp -f "$LAMP_PATH/config/mailpit.env" /etc/mailpit/mailpit.env
+    fi
+    cp -f "$LAMP_DISTRO_PATH/mailpit/mailpit.service" /lib/systemd/system/mailpit.service
+    systemctl daemon-reload
+    systemctl enable mailpit --now
+  else
+    console_log "Install script failed"
+    if [[ -f "/tmp/lamp-mailpit/install.log" ]]; then
+      while read -r line; do
+        console_log "$line"
+      done <"/tmp/lamp-mailpit/install.log"
+    fi
+  fi
+else
+  console_log "Download failed"
 fi
-mkdir -p /etc/mailpit
-chown "mailpit:mailpit" /etc/mailpit
-cp -f "$LAMP_DISTRO_PATH/mailpit/mailpit.env" /etc/mailpit/mailpit.env
-if [[ -f "$LAMP_PATH/config/mailpit.env" ]]; then
-  cp -f "$LAMP_PATH/config/mailpit.env" /etc/mailpit/mailpit.env
-fi
-cp -f "$LAMP_DISTRO_PATH/mailpit/mailpit.service" /lib/systemd/system/mailpit.service
-systemctl daemon-reload
-systemctl enable mailpit --now
+rm -rf "/tmp/lamp-mailpit"
