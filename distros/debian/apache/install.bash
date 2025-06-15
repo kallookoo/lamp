f21f686833a7405ec6ed0faa9c17fb4773f8bec2
kallookoo <kallookoo@users.noreply.github.com>
2025-06-15 14:44:02 +0200

    Move the apache (lamp) config to file to prevent the apache2.conf configurations

--- distros/debian/apache/install.bash
+++ distros/debian/apache/install.bash
@@ -12,7 +12,7 @@ fi
 LAMP_VIRTUALHOSTS_DIRECTORY="/$(sed -r 's@(^/|/$)@@g' <<<"${LAMP_VIRTUALHOSTS_DIRECTORY}")"
 if [ ! -d "$LAMP_VIRTUALHOSTS_DIRECTORY" ]; then
   mkdir -p "$LAMP_VIRTUALHOSTS_DIRECTORY"
-  chown -R "$SUDO_USER:$SUDO_USER" "$LAMP_VIRTUALHOSTS_DIRECTORY"
+  chown "$SUDO_USER:$SUDO_USER" "$LAMP_VIRTUALHOSTS_DIRECTORY"
 fi
 
 a2enmod -q \
@@ -29,24 +29,27 @@ a2enmod -q \
   setenvif \
   ssl
 
-cp -f "$LAMP_DISTRO_PATH/apache/apache2.conf" /etc/apache2/apache2.conf
 find /var/log/apache2 /etc/apache2/conf-enabled -mindepth 1 -delete
 find /etc/apache2/sites-enabled /etc/apache2/sites-available -mindepth 1 -name "*default*" -delete
-sed -i "s@__VIRTUALHOSTS_DIRECTORY__@$LAMP_VIRTUALHOSTS_DIRECTORY@g" /etc/apache2/apache2.conf
-sed -i "s/__PHP_VERSION__/$LAMP_PHP_VERSION/g" /etc/apache2/apache2.conf
-sed -i "s/__DEFAULT_DOMAIN__/$LAMP_FQDN/g" /etc/apache2/apache2.conf
+cp -f "$LAMP_DISTRO_PATH/apache/lamp.conf" /etc/apache2/lamp.conf
+if ! grep -q "Include lamp.conf" /etc/apache2/apache2.conf; then
+  sed -i -e '/^IncludeOptional conf-enabled\/\*.conf$/a\\n# Include the lamp configurations:\nInclude lamp.conf' /etc/apache2/apache2.conf
+fi
+sed -i "s@__VIRTUALHOSTS_DIRECTORY__@$LAMP_VIRTUALHOSTS_DIRECTORY@g" /etc/apache2/lamp.conf
+sed -i "s/__PHP_VERSION__/$LAMP_PHP_VERSION/g" /etc/apache2/lamp.conf
+sed -i "s/__DEFAULT_DOMAIN__/$LAMP_FQDN/g" /etc/apache2/lamp.conf
 
 LAMP_APACHE_ENABLE_MMAP="Off"
 if boolval "${LAMP_CONFIG_APACHE_ENABLE_MMAP:-no}"; then
   LAMP_APACHE_ENABLE_MMAP="On"
 fi
-sed -i "s/^EnableMMAP.*/EnableMMAP $LAMP_APACHE_ENABLE_MMAP/" /etc/apache2/apache2.conf
+sed -i "s/^EnableMMAP.*/EnableMMAP $LAMP_APACHE_ENABLE_MMAP/" /etc/apache2/lamp.conf
 
 LAMP_APACHE_ENABLE_SENDFILE="Off"
 if boolval "${LAMP_CONFIG_APACHE_ENABLE_SENDFILE:-no}"; then
   LAMP_APACHE_ENABLE_SENDFILE="On"
 fi
-sed -i "s/^EnableSendfile.*/EnableSendfile $LAMP_APACHE_ENABLE_SENDFILE/" /etc/apache2/apache2.conf
+sed -i "s/^EnableSendfile.*/EnableSendfile $LAMP_APACHE_ENABLE_SENDFILE/" /etc/apache2/lamp.conf
 
 if boolval "${LAMP_CONFIG_APACHE_ENABLE_H5BP:-yes}"; then
   bash "$LAMP_PATH/extras/h5bp-update" >/dev/null 2>&1
