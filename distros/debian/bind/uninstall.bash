0b0b29596ab51853ca59c4697a946d7aaa33172d
kallookoo <kallookoo@users.noreply.github.com>
2025-06-15 14:40:34 +0200

    Bind9 - Update uninstall message

--- distros/debian/bind/uninstall.bash
+++ distros/debian/bind/uninstall.bash
@@ -2,7 +2,7 @@
 # MailHog Uninstaller
 #
 
-console_log "As it is abandoned, any reference to it is eliminated."
+console_log "Remplaced by CoreDNS, any reference to it is eliminated."
 if [[ -f /etc/bind/lamp.conf ]]; then
   apt_remove bind9
   rm -rf /etc/bind
