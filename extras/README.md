9696b0a69f951770a145210a647300515f4790d5
kallookoo <kallookoo@users.noreply.github.com>
2025-09-06 23:05:35 +0200

    Update extras scripts

---
+++ extras/README.md
@@ -0,0 +1,4 @@
+# LAMP EXTRAS
+
+>[!NOTE]
+> Here are several scripts that I am creating. It is not recommended to use them unless you are 100% sure.
--- extras/systemd-hgfs.sh
+++ extras/systemd-hgfs.sh
@@ -1,53 +1,5 @@
 #!/usr/bin/env bash
 
-# Configure and enable the mounting of VMware shared folders on a Linux system using systemd.
-# Requirements: open-vm-tools
-#
-# Usage:
-# - bash systemd-hgfs.sh
-# - wget https://raw.githubusercontent.com/kallookoo/lamp/refs/heads/main/extras/systemd-hgfs.sh && bash systemd-hgfs.sh
-#
-# Info: https://knowledge.broadcom.com/external/article/316336/enabling-hgfs-shared-folders-on-fusion-o.html
-
-if [[ "$(id -u)" -ne "0" ]]; then
-  echo "This script must be run as root, please run this script again with the root user or sudo."
-  exit 1
-fi
-
-# Creating the fuse (open-vm-tools) configuration.
-if ! grep -qE '^fuse' /etc/modules-load.d/open-vm-tools.conf 2>/dev/null; then
-  echo "fuse" | tee -a /etc/modules-load.d/open-vm-tools.conf
-fi
-
-# Creating the default mount point.
-mkdir -p /mnt/hgfs
-
-# Disable the mount service if exists.
-[ -f /etc/systemd/system/mnt-hgfs.mount ] && systemctl disable mnt-hgfs.mount --now
-
-# Creating the mount service.
-# NOTE: This service set the default user/group in 1000 (DEBIAN and Derivatives)
-cat <<EOF >/etc/systemd/system/mnt-hgfs.mount
-[Unit]
-Description=VMware automount shared folders
-DefaultDependencies=no
-Before=umount.target
-ConditionVirtualization=vmware
-After=sys-fs-fuse-connections.mount
-
-[Mount]
-What=vmhgfs-fuse
-Where=/mnt/hgfs
-Type=fuse
-Options=default_permissions,allow_other,uid=1000,gid=1000
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Make sure the 'fuse' module is loaded
-modprobe -v fuse
-
-# Enable the mount service
-systemctl daemon-reload
-systemctl enable mnt-hgfs.mount --now
+echo "He moved to https://gist.github.com/kallookoo/09df0bb28aa3c7927fb40c474923a411"
+echo "To use it, run:"
+echo "curl -fSs https://gist.githubusercontent.com/kallookoo/09df0bb28aa3c7927fb40c474923a411/raw/systemd-hgfs.sh | sudo bash"
